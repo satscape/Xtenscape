800395a77d819b48b98667c91d5dd23665604d3d
Scott Hather <satscape@gmail.com>
2010-04-20 12:31:44 +0100

    first commit

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Xtenscape
+//
+//  Created by Scott Hather on 15/12/2008.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
